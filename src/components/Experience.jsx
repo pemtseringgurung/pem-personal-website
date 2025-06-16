4e8c92a2554e13d1832388782dee96a9f70ef1c7
Pem Tsering Gurung <pemgurung541@gmail.com>
2025-06-15 22:14:14 -0400

    Updated experience section with my new internship

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import cowImage from "../images/cow.png"
+import seLogo from "../images/se.png"
 
 const Experience = () => {
   const experiences = [
+    {
+      title: "Software Engineer Intern",
+      company: "Schneider Electric",
+      period: "May 2025 - Present",
+      description: "I engineered an agentic co-pilot using a Large Language Model (LLM) and LlamaIndex to translate natural language into database queries, which reduced client data retrieval time by 25%. I also designed a multi-agent architecture in Python, where a central Coordinator Agent delegated tasks to specialized Function Agents (Knowledge, Metrics, Invoice), improving query efficiency by 30%. Additionally, I implemented and trained an intelligent MCP client that dynamically selects tools from the MCP server and passes the necessary parameters, achieving 95% task automation. Further, I developed the central backend for all AI agent operations by building a multi-tool Model Context Protocol (MCP) server with FastAPI and containerized it using Docker.",
+      skills: ["Python", "FastAPI", "Docker", "Kubernetes", "Large Language Models (LLM)", "LlamaIndex", "Multi-Agent Systems", "Agentic Co-pilot", "Model Context Protocol (MCP)", "Natural Language Processing", "Database Query Optimization"],
+    },
+
     {
       title: "Digital Archive Research Assistant",
       company: "The College of Wooster",
@@ -52,10 +61,9 @@ const Experience = () => {
                     <span className="text-sm tracking-widest text-gray-500 dark:text-gray-400 uppercase">
                       {exp.period}
                     </span>
-                    {/* Image box */}
                     <div className="ml-4 w-12 h-12">
                       <img 
-                        src={cowImage} // Path to your cow image
+                        src={index === 0 ? seLogo : cowImage} // Path to your cow image
                         alt={`${exp.title} logo`} // Use a descriptive alt text
                         className="w-full h-full object-cover rounded-sm" 
                       />
